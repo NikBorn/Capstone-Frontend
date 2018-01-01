3d9ed7afe4d897f09132d6e60f16c71c2e51018c
Nik Bornemeier <NikolausRB@gmail.com>
2018-01-01 12:34:54 -0700

    add onClick for handling signin.

--- src/components/SignInSignUp/SignInSignUp.js
+++ src/components/SignInSignUp/SignInSignUp.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class SignInSignUp extends Component  {
 
+  signIn() {
+    console.log('sign in!')
+  }
+
   render() {
+    
     return (
-      <button>Sign In/Sign Up</button>
+      <button onClick={this.signIn}>Sign In/Sign Up</button>
     )
   }
 };
