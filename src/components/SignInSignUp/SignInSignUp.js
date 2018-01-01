7a2e814088f9c32107643dce425ca49fa114e797
Nik Bornemeier <NikolausRB@gmail.com>
2018-01-01 12:26:09 -0700

    add SignInSignUp Component.

---
+++ src/components/SignInSignUp/SignInSignUp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SignInSignUp extends Component  {
+
+  render() {
+    return (
+      <button>Sign In/Sign Up</button>
+    )
+  }
+};
+
+export default SignInSignUp;
