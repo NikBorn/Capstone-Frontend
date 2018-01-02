4e4d6772c2e25d19bcf79c8c6622360837159bab
Ben Porter <bbp5280@gmail.com>
2018-01-02 11:24:03 -0700

    soveed cores issue returning user upon sign in

--- src/components/SignInSignUp/SignInSignUp.js
+++ src/components/SignInSignUp/SignInSignUp.js
@@ -6,9 +6,12 @@ class SignInSignUp extends Component  {
   signIn() {
     auth.signInWithPopup(provider)
       .then((result) => {
-
-        console.log(result.user.displayName);
-      });
+        console.log(result.user.email)
+        fetch(`http://localhost:3000/api/v1/users/${result.user.email}`)
+          .then(response => response.json())
+          .then(parsed => console.log(parsed))
+      })
+      .catch(error => console.log(error))
   }
 
   render() {
