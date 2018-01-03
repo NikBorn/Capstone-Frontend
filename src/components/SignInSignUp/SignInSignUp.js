ad37e92a4c0201bc4a12b6625296f5168ca1b30b
Ben Porter <bbp5280@gmail.com>
2018-01-02 23:14:18 -0700

    add sign in user to the store

--- src/components/SignInSignUp/SignInSignUp.js
+++ src/components/SignInSignUp/SignInSignUp.js
@@ -8,52 +8,40 @@ import { withRouter } from 'react-router-dom';
 
 class SignInSignUp extends Component  {
   
-// signInUserToStore (response) {
-//   response.json()
-//     .then(parsed => {
-//       const user = {
-//         id: parsed[0].id,
-//         email: parsed[0].email,
-//         name: parsed[0].name,
-//         preferredLocation: parsed[0].preferredLocation
-//       };
-//         // console.log(user);
-//       console.log(signedInUser(user));
-//       this.props.signedInUser(user);
-//     });
-// };
+  signInUserToStore (response) {
+    response.json()
+      .then(parsed => {
+        const user = {
+          id: parsed[0].id,
+          email: parsed[0].email,
+          name: parsed[0].name,
+          preferredLocation: parsed[0].preferredLocation
+        };
+        this.props.signedInUser(user);
+      });
+  }
 
-signIn() {
-  auth.signInWithPopup(provider)
-    .then((result) => {
-      fetch(`http://localhost:3000/api/v1/users/${result.user.email}`)
-        .then(response => {
-          if (response.status === 200){
-            response.json()
-              .then(parsed => {
-                const user = {
-                  id: parsed[0].id,
-                  email: parsed[0].email,
-                  name: parsed[0].name,
-                  preferredLocation: parsed[0].preferredLocation
-                };
-                // console.log(user);
-                console.log(signedInUser(user))
-                signedInUser(user);
-              });
-            // signInUserToStore(response);
-          }
-        });
-    })
-    .catch(error => console.log(error));
-}
+  signIn() {
+    auth.signInWithPopup(provider)
+      .then((result) => {
+        fetch(`http://localhost:3000/api/v1/users/${result.user.email}`)
+          .then(response => {
+            if (response.status === 200){
+              this.signInUserToStore(response);
+            } else {
+              console.log(result.user)
+            }
+          });
+      })
+      .catch(error => console.log(error));
+  }
 
-render() {
-  console.log(this.props);
-  return (
-    <button onClick={this.signIn}>Sign In/Sign Up</button>
-  );
-}
+  render() {
+    console.log(this.props);
+    return (
+      <button onClick={this.signIn.bind(this)}>Sign In/Sign Up</button>
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
@@ -64,11 +52,5 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-// const mapStateToProps = (state) => {
-//   return {
-//     userCoords: state.userCoords,
-//     locationConcerts: state.locationConcerts
-//   };
-// };
 
 export default withRouter(connect(null, mapDispatchToProps)(SignInSignUp));
