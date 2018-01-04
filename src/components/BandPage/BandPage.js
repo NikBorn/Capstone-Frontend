2788d59eb8f44eb30edf470fa0fb60b23d5f27be
Nik Bornemeier <NikolausRB@gmail.com>
2018-01-04 16:39:41 -0700

    remove console logs and white space

--- src/components/BandPage/BandPage.js
+++ src/components/BandPage/BandPage.js
@@ -2,31 +2,15 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-
 const BandPage = (props) => {
 
-  const showsArray = props.searchBandResults;
-
-  // console.log(shows)
-  
+  const showsArray = props.searchBandResults; 
   const shows = showsArray.map(show => {
-    // console.log(show.)
-
     const venue = show.entities ? show.entities.venues[0].name : 'No venue name';
-    const month = show.start.substr(5, 2)
-    const year = show.start.substr(0, 4)
-    const day = show.start.substr(8, 2)
-    // const showDate = (show.start) => {
-      const showDate = month.concat(`-${day}-`, year)
-      // const showDateCorrect = showDate.substr(0, 10);
-      // const year = showDate.substr(0, 4);
-      // // const
-      // return show.start
-    // }
-    
-    
-    
-    console.log(showDate)
+    const month = show.start.substr(5, 2);
+    const year = show.start.substr(0, 4);
+    const day = show.start.substr(8, 2);
+    const showDate = month.concat(`-${day}-`, year);
 
     return (
       <div className='show-card'>
@@ -34,15 +18,15 @@ const BandPage = (props) => {
         <h4>{venue}</h4>
         <h4>Date: {showDate}</h4>
       </div>
-    )
-  })
+    );
+  });
 
   return (
     <div>
       {shows}
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -50,6 +34,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-
-
-export default withRouter(connect(mapStateToProps, null)(BandPage))
+export default withRouter(connect(mapStateToProps, null)(BandPage));
