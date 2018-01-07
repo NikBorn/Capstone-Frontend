7c3a32752b5aefee54af41b32e9e044454b75f95
Nik Bornemeier <NikolausRB@gmail.com>
2018-01-07 12:53:20 -0700

    try to fix error

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'app'),
     filename: 'bundle.js',
-    publicPath: '/app/'
+    publicPath: '/src/'
   },
   module: {
     loaders: [
