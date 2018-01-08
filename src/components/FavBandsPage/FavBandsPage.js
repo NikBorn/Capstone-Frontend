56621ecc826ae51353263e54e7494b6bb57f6df0
Nik Bornemeier <NikolausRB@gmail.com>
2018-01-07 18:38:44 -0700

    add functionality to return band objects from API

---
+++ src/components/FavBandsPage/FavBandsPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FavBandsPage extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <h3>Favorite Bands</h3>
+      </div>
+    )
+  }
+}
+
+export default FavBandsPage;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import { connect } from 'react-redux';
+
 
 class NavBar extends Component {
+
+  fetchFavBands() {
+    console.log(this.props.signedInUser.id)
+    fetch(`http://localhost:3000/api/v1/users/${this.props.signedInUser.id}/favorite_bands`)
+      .then(response => response.json())
+      .then(parsed => {
+        console.log(parsed)
+        let bands = parsed.map(async (bandObj) => {
+          return await this.fetchBandName(bandObj.id)
+        })
+        console.log(bands)
+        return Promise.all(bands)
+      })
+      .then(res => console.log(res))
+      .catch(error => console.log(error))
+  }
+
+  fetchBandName(bandId) {
+    return (
+      fetch(`http://localhost:3000/api/v1/bands/${bandId}`)
+        .then(response => response.json())
+        .catch(error => console.log(error))
+    )
+  }
 
   render () {
     return (
       <ul className='NavBar'>
         <li>
-          <Link to='/favorite-bands'>
-            Favorite Bands
-          </Link>
+          <button onClick={(event) => {
+            event.preventDefault()
+            this.fetchFavBands();
+          }}>
+            <Link to='/favorite-bands'>
+              Favorite Bands
+            </Link>
+          </button>
         </li>
         <li>
           <Link to='/favorite-shows'>
@@ -22,4 +53,10 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar;
+const mapStateToProps = (state) => {
+  return {
+    signedInUser: state.signedInUser
+  };
+};
+
+export default connect(mapStateToProps, null)(NavBar);
