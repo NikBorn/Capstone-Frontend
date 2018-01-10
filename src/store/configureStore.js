afe8653bb21a94474d224bbede1e343d8136b9d7
Nik Bornemeier <NikolausRB@gmail.com>
2018-01-10 09:52:56 -0700

    get CircleCi to pass

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,6 +4,7 @@ import rootReducer from '../reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
